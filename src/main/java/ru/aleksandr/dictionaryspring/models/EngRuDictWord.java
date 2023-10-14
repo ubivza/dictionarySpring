a76341e3ebd80c22cdfcefb24de1c83ae7935853
Aleksandr Belov <uka_ka@inbox.ru>
2023-10-14 14:47:48 +0300

    small fixes

--- src/main/java/ru/aleksandr/dictionaryspring/models/EngRuDictWord.java
+++ src/main/java/ru/aleksandr/dictionaryspring/models/EngRuDictWord.java
@@ -1,6 +1,5 @@
 package ru.aleksandr.dictionaryspring.models;
 
-import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Data;
@@ -8,7 +7,6 @@ import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@Valid
 public class EngRuDictWord {
     @Pattern(regexp = "[0-9]{5}", message = "Word must be 5 characters long and should contain only numbers")
     @NotBlank(message = "Word shouldn't be empty")
